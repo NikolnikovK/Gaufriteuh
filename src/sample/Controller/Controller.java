8d73efa2fb6976b845a91cd6b8ea9781342f6353
secardia <aurelien.secardin@icloud.com>
2018-04-26 18:22:19 +0200

    Version texte jouable et stable

--- src/sample/Controller/Controller.java
+++ src/sample/Controller/Controller.java
@@ -1,12 +1,29 @@
 package src.sample.Controller;
 
 import src.sample.Model.Grid;
+import java.util.Scanner;
 
 public class Controller {
 
     public Controller() {
         Grid g = new Grid(5,5,0,0);
         g.printGrid();
+
+        Scanner sc = new Scanner(System.in);
+        int line, column;
+        System.out.println("Veuillez saisir un numéro de ligne :");
+        line = sc.nextInt();
+        System.out.println("Veuillez saisir un numéro de colonne :");
+        column = sc.nextInt();
+
+        while (g.play(line,column) == 0) {
+            g.printGrid();
+            System.out.println("Veuillez saisir un numéro de ligne :");
+            line = sc.nextInt();
+            System.out.println("Veuillez saisir un numéro de colonne :");
+            column = sc.nextInt();
+        }
+
     }
 
 }
